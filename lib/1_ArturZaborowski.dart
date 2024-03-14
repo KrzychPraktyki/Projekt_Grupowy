0b2894a8300fa783a0c12e418e410e6183cb398a
SzeregowyPraktykant <161303522+SzeregowyPraktykant@users.noreply.github.com>
2024-03-14 13:24:42 +0100

    Update and rename 1.dart to 1_ArturZaborowski.dart

---
+++ lib/1_ArturZaborowski.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'language_changer_ArturZaborowski.dart';
+
+void main() {
+  runApp(PierwszaStrona());
+}
+
+class PierwszaStrona extends StatelessWidget {
+  const PierwszaStrona({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wybierz Język',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LanguageChanger(child: Door()),
+    );
+  }
+}
+
+class Door extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Door Page'),
+      ),
+      body: Center(
+        child: Text('This is the Door Page'),
+      ),
+    );
+  }
+}
