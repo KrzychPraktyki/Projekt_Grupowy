40083649ddeb288ba83921e8a5029f27b8751dc0
Kochuroff <161301105+Kochuroff@users.noreply.github.com>
2024-03-19 10:26:02 +0100

    Update Danylo Kochurov.dart

--- lib/Danylo Kochurov.dart
+++ lib/Danylo Kochurov.dart
@@ -1,26 +1,93 @@
 import 'package:flutter/material.dart';
+
 class TrzeciaStrona extends StatelessWidget {
-  const TrzeciaStrona({super.key});
+  const TrzeciaStrona({Key?key}) :super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style =
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 18));
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task Manager'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: style,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('back!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TaskManager(),
     );
   }
 }
+class TaskManager extends StatefulWidget {
+  @override
+  _TaskManager createState() => _TaskManager();
+
+}
+
+class _TaskManager extends State<TaskManager> {
+bool? isCheked1 = false;
+bool? isCheked2 = false;
+bool? isCheked3 = false;
+bool? isCheked4 = false;
+bool? isCheked5 = false;
+bool? isCheked6 = false;
+var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Menadzer Zadan'),
+      ),
+      body:Column(children: [
+        Row(
+          children: [
+            if (counter>0) Checkbox(value: isCheked1, onChanged: (newBool){
+              setState(() {isCheked1 = newBool;});}),
+            if (counter>0) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+        Row(
+          children: [
+            if (counter>1) Checkbox(value: isCheked2, onChanged: (newBool){
+              setState(() {isCheked2 = newBool;});}),
+            if (counter>1) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+        Row(
+          children: [
+            if (counter>2) Checkbox(value: isCheked3, onChanged: (newBool){
+              setState(() {isCheked3 = newBool;});}),
+            if (counter>2) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+        Row(
+          children: [
+            if (counter>3) Checkbox(value: isCheked4, onChanged: (newBool){
+              setState(() {isCheked4 = newBool;});}),
+            if (counter>3) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+        Row(
+          children: [
+            if (counter>4) Checkbox(value: isCheked5, onChanged: (newBool){
+              setState(() {isCheked5 = newBool;});}),
+            if (counter>4) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+        Row(
+          children: [
+            if (counter>5) Checkbox(value: isCheked6, onChanged: (newBool){
+              setState(() {isCheked6 = newBool;});}),
+            if (counter>5) SizedBox(height: 100,width: 300,child: TextField(style: TextStyle(fontSize: 34),),),
+          ],
+        ),
+
+      ],),
+      floatingActionButton:Row(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,children: [
+        FloatingActionButton(
+          onPressed: (){setState((){if(counter==0){null;} else{counter--;};print(counter);});}, child: Icon(Icons.remove),),
+        FloatingActionButton(
+          onPressed: (){setState((){counter=0;print(counter);});}, child: Icon(Icons.restart_alt),),
+        FloatingActionButton(
+          onPressed: (){setState((){counter++;print(counter);});}, child: Icon(Icons.add),),
+      ],),
+    );
+  }
 
 
+}
